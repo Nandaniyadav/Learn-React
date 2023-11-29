8cf9a7a09cd911499777e97026c5dc6ab92f54e6
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-11-29 10:07:43 +0530

    JSX Attributes in ReactJS

--- src/By Thapa/Challange2.js
+++ src/By Thapa/Challange2.js
@@ -1,14 +1,36 @@
+
+// import React from "react";
+// const Challange2 = () => {
+//   let name = "Nandani Yadav";
+//   let curdate = new Date().toLocaleDateString();
+//   let curtime = new Date().toLocaleTimeString();
+//   return (
+//     <div>
+//       <h1 contentEditable>My Name is {name}</h1>
+//       <h3>Today date is : {curdate}</h3>
+//       <h3>current Time is {curtime}</h3>
+//     </div>
+//   );
+// };
+// export default Challange2;
+
+
+// ////////////////////////////////////////////////
+
 import React from "react";
-const Challange2 = () => {
-  let name = "Nandani Yadav";
-  let curdate = new Date().toLocaleDateString();
-  let curtime = new Date().toLocaleTimeString();
-  return (
+const Challange2=()=>{
+  let img1 = "https://picsum.photos/200/300"
+  let img2 = "https://picsum.photos/250/300"
+  let img3 = "https://picsum.photos/300/300"
+return(
+  <div>
+    <h1 className="head"> My name is Nandani Yadav</h1>
     <div>
-      <h1>My Name is {name}</h1>
-      <h3>Today date is : {curdate}</h3>
-      <h3>current Time is {curtime}</h3>
+      <img src={img1} alt="Randam Images"></img>
+      <img src={img2} alt="Randam Images"></img>
+      <img src={img3} alt="Randam Images"></img>
     </div>
-  );
-};
+  </div>
+)
+}
 export default Challange2;
